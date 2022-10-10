375292b67c808fd4033e9ec1458b80c9254f0c42
dabrorius <filip.defar@protonmail.com>
2022-10-10 11:38:40 +0200

    feat: Add depth chart areas

--- src/components/DepthChart.tsx
+++ src/components/DepthChart.tsx
@@ -1,9 +1,9 @@
 import { scaleLinear } from "d3-scale";
-import { extent, max } from "d3-array";
+import { extent, max, range } from "d3-array";
 import { BitstampHttpClient } from "../api/BitstampHttpClient";
 import { useEffect, useRef, useState } from "react";
 import { OrderBookEntry } from "../api/BitstampSocketClient";
-import { line } from "d3-shape";
+import { area, line } from "d3-shape";
 
 type CumulativeBookEntry = {
   sum: number;
@@ -11,14 +11,18 @@ type CumulativeBookEntry = {
   amount: number;
 };
 
-function parseAndCumulate(entries: OrderBookEntry[]): CumulativeBookEntry[] {
+function parseAndCumulate(
+  entries: OrderBookEntry[],
+  lowLimit: number,
+  highLimit: number
+): CumulativeBookEntry[] {
   let sum = 0;
   return entries
     .map(([valueString, amountString]) => ({
       value: parseFloat(valueString),
       amount: parseFloat(amountString),
     }))
-    .filter((entry) => entry.value > 900 && entry.value < 2000)
+    .filter((entry) => entry.value > lowLimit && entry.value < highLimit)
     .map((entry) => {
       sum += entry.amount;
       return { ...entry, sum };
@@ -34,11 +38,16 @@ export function DepthChart() {
     height: 0,
   };
 
+  const midPrice = 1300;
+  const rangePercent = 0.1;
+  const highLimit = midPrice * (1 + rangePercent);
+  const lowLimit = midPrice * (1 - rangePercent);
+
   const [bids, setBids] = useState<OrderBookEntry[]>([]);
-  const cumulativeBids = parseAndCumulate(bids);
+  const cumulativeBids = parseAndCumulate(bids, lowLimit, highLimit);
 
   const [asks, setAsks] = useState<OrderBookEntry[]>([]);
-  const cumulativeAsks = parseAndCumulate(asks);
+  const cumulativeAsks = parseAndCumulate(asks, lowLimit, highLimit);
 
   const fetchBidsAndAsks = () => {
     BitstampHttpClient.fetchOrderBook()
@@ -69,23 +78,42 @@ export function DepthChart() {
   const amounts = bidAmounts.concat(askAmounts);
   const maxAmount = max(amounts) || 0;
 
-  const scaleY = scaleLinear().domain([0, maxAmount]).range([height, 0]);
+  const scaleY = scaleLinear()
+    .domain([0, maxAmount * 1.2])
+    .range([height, 0]);
 
-  const lineDatagGenerator = line<CumulativeBookEntry>()
+  const lineDataGenerator = line<CumulativeBookEntry>()
     .x((d) => scaleX(d.value))
     .y((d) => scaleY(d.sum));
+
+  const areaDataGenerator = area<CumulativeBookEntry>()
+    .x((d) => scaleX(d.value))
+    .y0(() => height)
+    .y1((d) => scaleY(d.sum));
 
   return (
     <svg width="100%" height="200" ref={svgRef}>
       <path
-        d={lineDatagGenerator(cumulativeBids) || undefined}
+        d={lineDataGenerator(cumulativeBids) || undefined}
         stroke="green"
         fill="none"
       />
       <path
-        d={lineDatagGenerator(cumulativeAsks) || undefined}
+        d={areaDataGenerator(cumulativeBids) || undefined}
+        stroke="none"
+        fill="green"
+        opacity="0.3"
+      />
+      <path
+        d={lineDataGenerator(cumulativeAsks) || undefined}
         stroke="red"
         fill="none"
+      />
+      <path
+        d={areaDataGenerator(cumulativeAsks) || undefined}
+        stroke="none"
+        fill="red"
+        opacity="0.3"
       />
     </svg>
   );
