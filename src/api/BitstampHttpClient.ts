790417d2e93c5305deb98b09c290db33756b86b0
dabrorius <filip.defar@protonmail.com>
2022-10-16 13:04:01 +0200

    feat: Use proxy to fetch trades

--- src/api/BitstampHttpClient.ts
+++ src/api/BitstampHttpClient.ts
@@ -1,14 +1,10 @@
 import axios from "axios";
 
+const rootUrl =
+  "https://h0hoyraq17.execute-api.us-east-1.amazonaws.com/default";
+
 export const BitstampHttpClient = {
-  fetchOrderBook: () =>
-    axios.get(
-      "https://h0hoyraq17.execute-api.us-east-1.amazonaws.com/default/BitStampOrderBook"
-    ),
-  fetchTicker: () =>
-    axios.get(
-      "https://h0hoyraq17.execute-api.us-east-1.amazonaws.com/default/BitStampTicker"
-    ),
-  fetchTrades: () =>
-    axios.get("https://www.bitstamp.net/api/v2/transactions/ethusd/?time=day"),
+  fetchOrderBook: () => axios.get(`${rootUrl}/BitStampOrderBook`),
+  fetchTicker: () => axios.get(`${rootUrl}/BitStampTicker`),
+  fetchTrades: () => axios.get(`${rootUrl}/BitStampTransactions`),
 };
